f29f4ba44ce512440a2c586881356da2b45cb86a
Lampadare <ml2419@ic.ac.uk>
2024-06-02 03:24:23 +0100

    things happened and other thngs didn't but we cooking, openbci processing

--- API/testAPI.js
+++ API/testAPI.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const testData = {
   concept: "Newton's Second Law",
@@ -6,16 +6,20 @@ const testData = {
   components: [
     { name: "Force", anchor: "Police" },
     { name: "Mass", anchor: "Your Mum" },
-    { name: "Acceleration", anchor: "running" }
-  ]
+    { name: "Acceleration", anchor: "running" },
+  ],
 };
 
-axios.post('http://localhost:3000/generate-mnemonic', testData)
-  .then(response => {
-    console.log('Analogy:', response.data.analogy);
-    console.log('Story:', response.data.story);
+axios
+  .post("http://localhost:3000/generate-mnemonic", testData)
+  .then((response) => {
+    console.log("Analogy:", response.data.analogy);
+    console.log("Story:", response.data.story);
   })
-  .catch(error => {
-    console.error('Error making API call:', error.message);
-    console.log('Detailed error:', error.response ? error.response.data : 'No additional error data');
+  .catch((error) => {
+    console.error("Error making API call:", error.message);
+    console.log(
+      "Detailed error:",
+      error.response ? error.response.data : "No additional error data"
+    );
   });
